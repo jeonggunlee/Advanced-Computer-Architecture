58927697bbb113f02556d3207415615502977678
Jeong-Gun Lee <jeonggun.lee@gmail.com>
2018-03-22 00:51:34 +0900

    Update add32.v

--- single_cycle_mips/add32.v
+++ single_cycle_mips/add32.v
@@ -1,20 +1,7 @@
 //-----------------------------------------------------------------------------
-// Title         : ALU Behavioral Model
-// Project       : ECE 313 - Computer Organization
+// Title         : ##32-bit Adder Behavioral Model##
 //-----------------------------------------------------------------------------
-// File          : add32.v
-// Author        : John Nestor  <nestorj@lafayette.edu>
-// Organization  : Lafayette College
-// 
-// Created       : October 2002
-// Last modified : 7 January 2005
-//-----------------------------------------------------------------------------
-// Description :
-//   Behavioral model of 32-bit adder  used in the implementations of the MIPS processor
-//   subset described in Ch. 5-6 of "Computer Organization and Design, 3rd ed."
-//   by David Patterson & John Hennessey, Morgan Kaufmann, 2004 (COD3e).  
-//
-//-----------------------------------------------------------------------------
+
 
 module add32(a, b, result);
   input [31:0] a, b;
